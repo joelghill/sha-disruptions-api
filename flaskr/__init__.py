b1a15ee2b30c37faa4f69a9d63ef47b59beb3f2d
Joel Hill <joel.hill@solusguard.com>
2022-07-30 23:06:57 -0600

    Fixed swagger and cache

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-import json
+from datetime import datetime, timedelta, timezone
 
 from flask import Flask, request, jsonify
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -38,8 +38,8 @@ def create_app(test_config=None):
 
     # Initialize Swagger
     app.json_encoder = LazyJSONEncoder
-    template = dict(
-        info={
+    template = {
+        "info": {
             "title": "SHA Service Disruptions",
             "description": "Unofficial API used to retrieve information about communities with health service disruptions in Saskatchewan",
             "contact": {
@@ -49,28 +49,66 @@ def create_app(test_config=None):
             },
             "version": "1.0.0",
         },
-        host=LazyString(lambda: request.host),
-        schemes=[LazyString(lambda: "https" if request.is_secure else "http")],
-        basePath="",  # base bash for blueprint registration
-    )
+        "host": LazyString(lambda: request.host),
+        "schemes": [LazyString(lambda: "https" if request.is_secure else "http")],
+        "basePath": "",  # base bash for blueprint registration
+    }
 
-    # Initialize swagger documentation
-    Swagger(app, template=template)
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "v1",
+                "route": "/v1.json",
+                "rule_filter": lambda rule: True,  # all in
+                "model_filter": lambda tag: True,  # all in
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        # "static_folder": "static",  # must be set by user
+        "swagger_ui": True,
+        "specs_route": "/v1/",
+        "swagger": "2.0"
+    }
 
     # Initialize Cache
     cache = Cache(app)
 
+    # Initialize swagger documentation
+    Swagger(app, template=template, config=swagger_config)
+
     # Create Endpoint
     @app.route("/v1/disruptions")
-    @cache.cached(timeout=86400)
     def service_disruptions():
         """
         file: ../openapi/disruptions.yml
         """
-        disruptions = get_all_disruptions(URL)
+        now = datetime.now(tz=timezone.utc)
+        # get the last cache
+        last_accessed = cache.get("last_cache")
+
+        # If there was a cached date
+        if last_accessed:
+            # compare to current time
+            difference: timedelta = now - last_accessed
+
+            # If it's been more than a day...
+            if difference.days >= 1:
+                # get the disruptions
+                disruptions = get_all_disruptions(URL)
+                # cache current time and disruptions
+                cache.set("last_cache", now)
+                cache.set("disruptions", disruptions)
+            else:
+                # Get from cache
+                disruptions = cache.get("disruptions")
+        else:
+            # No cached data
+            disruptions = get_all_disruptions(URL)
+            # cache current time and disruptions
+            cache.set("last_cache", now)
+            cache.set("disruptions", disruptions)
 
         return jsonify(disruptions)
 
     return app
-
-
